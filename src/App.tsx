723387a380924ead119ee9fdeac24a52e8a33a2b
Yury Daszukiewicz <ydaszukiewicz@gmail.com>
2022-11-10 18:16:37 +0300

    Commit-samurai-way - 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
-import {Technologies} from "./Technologies";
-import {Header} from "./Header";
+import {Header} from "./components/Header";
+import {NavBar} from "./components/NavBar";
+import {Profile} from "./components/Profile";
 
 const App = () => {
     return (
-        <>
-            <Technologies/>
+        <div className='app-wrapper'>
+
             <Header/>
-        </>
+            <NavBar/>
+            <Profile/>
+
+        </div>
     );
 }
 
 export default App;
+
+
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export const Header = () => {
+    return (
+        <>
+        <header className='header'>
+            <img src="../img/logo/LogoStart.png" alt="just logo"/>
+        </header>
+        </>
+    );
+}
