9ee998308d1f2a81b3aec95e901a380855320c8f
Yury Daszukiewicz <ydaszukiewicz@gmail.com>
2022-11-19 15:00:41 +0300

    Commit-samurai-way - 19

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,33 @@ import {Header} from "./components/Header/Header";
 import {NavBar} from "./components/NavBar/NavBar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs"
+import {Route, Routes} from "react-router-dom";
+import {Settings} from "./components/Settings/Settings";
+import {Music} from "./components/Music/Music";
+import {News} from "./components/News/News";
 
 const App = () => {
-    return (
-        <div className='app-wrapper'>
 
+    return (
+
+        <div className='app-wrapper'>
             <Header/>
             <NavBar/>
-            {/*<Profile/>*/}
-
             <div className="app-wrapper-content">
+                <Routes>
 
-                <Dialogs/>
+                    <Route path='/profile' element={<Profile/>}/>
+                    <Route path='/dialogs' element={<Dialogs/>}/>
+                    <Route path='/news' element={<News/>}/>
+                    <Route path='/music' element={<Music/>}/>
+                    <Route path='/settings' element={<Settings/>}/>
 
+                </Routes>
             </div>
+
         </div>
-    );
+
+    )
 }
 
 export default App;
